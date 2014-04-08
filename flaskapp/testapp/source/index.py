8e5639519e6b2fb619c84c6a088157a772c05b1e
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-04-08 16:05:44 +0900

    ログイン機構をつけてみる

--- flaskapp/testapp/source/index.py
+++ flaskapp/testapp/source/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
-from flask import render_template
-from flask import abort, redirect, url_for
+from flask import Flask, abort, redirect, url_for, render_template, session, escape, request
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Index Page'
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+        <p><input type=text name=username>
+        <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if its there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 @app.route('/hello')
 @app.route('/hello/<name>')
@@ -24,16 +42,19 @@ def show_post(post_id):
 
 @app.route('/redirect_test')
 def redirect_test():
-    return redirect(url_for('login'))
+    return redirect(url_for('error'))
 
-@app.route('/login')
-def login():
+@app.route('/error')
+def error():
     abort(404)
     this_is_never_executed()
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+#ほんとはあげちゃダメだよ
+app.secret_key = '\x08Rv\xa4\r\xc4\x17\xf8\xbc:N\xcdV\x18\x84\xe2\xac&\xb0\xd4\x8b\x1c^4'
 
 if __name__ == '__main__':
     app.run()
