231854ec5c308035709821901c8669c7c682f55f
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-04-09 17:50:23 +0900

    ディレクトリ構成の修正

--- flaskapp/flaskr/flaskr/__init__.py
+++ flaskapp/flaskr/flaskr/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 
 # create our little application :)
 app = Flask(__name__)
+import flaskr.views
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -39,12 +40,6 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
 @app.route('/')
 def show_entries():
     db = get_db()
@@ -63,25 +58,11 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 if __name__ == '__main__':
     init_db()
---
+++ flaskapp/flaskr/flaskr/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flaskr import app
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
