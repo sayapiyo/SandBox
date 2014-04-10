be36b5c17e3895f29659a52ecc3bdb7fe757d128
SayaTsutsumi <saya.tsutsumi@gmail.com>
2014-04-10 15:57:58 +0900

    configファイルの切り出し

--- flaskapp/flaskr/flaskr/__init__.py
+++ flaskapp/flaskr/flaskr/__init__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
-import os
-from sqlite3 import dbapi2 as sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-         render_template, flash
+from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # create our little application :)
 app = Flask(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_object('flaskr.config')
 db = SQLAlchemy(app)
 import flaskr.views
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskr.db',
-    SECRET_KEY='development key',
-    DEBUG=True,
-    USERNAME='admin',
-    PASSWORD='default'
-    ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 if __name__ == '__main__':
     init_db()
---
+++ flaskapp/flaskr/flaskr/config.py
@@ -0,0 +1,6 @@
+# Load default config and override config from an environment variable
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskr.db'
+SECRET_KEY='development key'
+DEBUG=True
+USERNAME='admin'
+PASSWORD='default'
