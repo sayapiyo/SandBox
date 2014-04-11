380d886daae35bf299bdfee839f95a7670996351
lateau <lateau@gmail.com>
2014-04-11 18:22:19 +0900

    Update sqlite_sample.py

--- sqlite_sample.py
+++ sqlite_sample.py
@@ -1,20 +1,32 @@
-#!/usr/bin/enc python2.7
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
 import sqlite3
-#郵便番号のリスト
+
+"""
+sqlite3 のテストコード。
+sqlite3 のコネクションを取得し、テーブル作成から検索までを行う。
+"""
+
+# 郵便番号のリスト
 postno = [[u"東京都千代田区", "100-0000"],
-		  [u"東京都千代田区飯田橋", "102-0072"],
-		  [u"東京都千代田区一番町", "102-0082"],
-		  [u"東京都千代田区岩本町", "101-0032"], ]
-# コネクションオブジェクト作る
+          [u"東京都千代田区飯田橋", "102-0072"],
+          [u"東京都千代田区一番町", "102-0082"],
+          [u"東京都千代田区岩本町", "101-0032"], ]
+
+# コネクションオブジェクト作る。
 con = sqlite3.connect(":memory:")
 cur = con.cursor()
-cur.execute("""CREATE TABLE postdb(
-				address text,postno text)""")
+
+# postdb テーブルの初期化。
+cur.execute('CREATE TABLE postdb(address text, postno text)')
 for item in postno:
-		cur.execute("""INSERT INTO postdb(address,postno)
-			VALUES(?,?)""",item)
-cur.execute("SELECT * FROM postdb WHERE postno =?",("101-0032",))
+    cur.execute('INSERT INTO postdb(address,postno) VALUES(?,?)', item)
+
+# postdb からレコード１件を検索し、stdout に出力する。
+cur.execute('SELECT * FROM postdb WHERE postno = ?', ("101-0032",))
 data = cur.fetchone()
-print data[1],data[0]
+print data[1].encode('utf-8'), data[0].encode('utf-8')
+
+# コネクションを切る。
+con.close()
