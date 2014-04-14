8f1e3419c47f0fd392c4777e05490dd1f74b3103
lateau <lateau@gmail.com>
2014-04-14 13:47:04 +0900

    Update complete.py

--- complete.py
+++ complete.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python2.4
 # -*- coding: utf-8 -*-
 
-def get_divisor(number):
+def get_divisors(number):
     """ 引数 number の約数をリストにして返す。
     >>> import complete
-    >>> complete.get_divisor(6)
+    >>> complete.get_divisors(6)
     [1, 2, 3]
-    >>> complete.get_divisor(7)
+    >>> complete.get_divisors(7)
     [1]
-    >>> complete.get_divisor(100)
+    >>> complete.get_divisors(100)
     [1, 2, 4, 5, 10, 20, 25, 50]
     """
     l = [cnt for cnt in range(1, number) if (number % cnt) == 0]
     return l
 
+# TODO: 名称的には perfect number が正しい
+#
 def is_complete(number):
     """ 数値が完全数かどうかを調べ、結果を bool 値で返す。"""
-    multi = 0
-    for num in get_divisor(number):
-        multi += num
-    if multi == number:
+    divisors = get_divisors(number)
+    if sum(divisors) == number:
         return True;
     return False;
 
