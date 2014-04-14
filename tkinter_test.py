da23e1f9eb1754fae8342b5f96591cb1c8544879
lateau <lateau@gmail.com>
2014-04-14 15:45:25 +0900

    Update tkinter_test.py

--- tkinter_test.py
+++ tkinter_test.py
@@ -1,34 +1,44 @@
 #! /usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
-from Tkinter import *
+from Tkinter import (
+    Frame, Label, Button,
+    TOP, LEFT, RIGHT,
+    StringVar
+)
 import time
 
 class App(Frame):
-	TIME = 60 * 3
-	def __init__(self, master = None):
-		Frame.__init__(self,master)
-		self.time = 0
-		self.master.title("Test Timer")
-		self.timestr = StringVar()
-		self.timestr.set("03:00")
-		l = Label(self,textvariable=self.timestr,
-				font=('Helvetica', '48', 'bold'))
-		b1 = Button(self,text="Start", command=self.countdown)
-		b2 = Button(self,text="Quit",
-					command=self.master.destroy)
-		for obj,sideparam in ((l,TOP),(b1,LEFT),(b2,RIGHT)):
-				obj.pack(side=sideparam)
-		self.pack()
+    TIME = 10 * 1
+    TIME_FORMAT = "%02d:%02d"
 
-	def countdown(self):
-		if self.time == 0:
-			self.time = time.time()
-		timeleft = max(self.TIME-(time.time()-self.time), 0)
-		min,sec = (timeleft) / 60, timeleft % 60
-		self.timestr.set("%02d:%02d" % (min, sec))
-		self.after(1000,self.countdown) #１秒後にメソッドを呼ぶ
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.time = 0
+        self.master.title("Test Timer")
+        self.timestr = StringVar()
+        self.timestr.set("03:00")
+        title = Label(self,textvariable=self.timestr,
+                      font=('Helvetica', '48', 'bold'))
+        start_btn = Button(self,text="Start", command=self.countdown)
+        quit_btn = Button(self,text="Quit", command=self.master.destroy)
+        for obj, sideparam in ((title,TOP),(start_btn,LEFT),(quit_btn,RIGHT)):
+            obj.pack(side=sideparam)
+        self.pack()
+
+    def countdown(self):
+        if self.time == 0:
+            self.time = time.time()
+        timeleft = self.TIME - (time.time() - self.time)
+        timeleft = max(timeleft, 0)
+        min, sec = ((timeleft) / 60, timeleft % 60)
+        self.timestr.set(self.TIME_FORMAT % (min, sec))
+        if 1 > timeleft:
+            return
+        if __debug__:
+            print 'timeleft: %d' % timeleft
+        self.after(1 * 1000, self.countdown) #１秒後にメソッドを呼ぶ
 
 if __name__ == "__main__":
-		app = App()
-		app.mainloop()
+    app = App()
+    app.mainloop()
